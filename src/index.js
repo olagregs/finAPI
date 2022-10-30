4a17f3f646bfb53160aad049c8fd0c1efc10cad6
Greg <gregoryflucena@gmail.com>
2022-10-30 13:07:44 -0300

    Projeto inicial ignite

---
+++ src/index.js
@@ -0,0 +1,142 @@
+const express = require('express');
+const { v4: uuid } = require("uuid");
+
+const app = express();
+
+app.use(express.json());
+
+const customers = [];
+
+function verifyAccount(request, response, next) {
+  const { cpf } = request.headers;
+
+  const customer = customers.find(customer => customer.cpf === cpf);
+
+  if (!customer) {
+    return response.status(400).json({ error: "Customer not found" });
+  }
+
+  request.customer = customer;
+
+  return next();
+}
+
+function getBalance(statement) {
+  const balance = statement.reduce((acc, operation) => {
+    if (operation.type === 'credit') {
+      return acc + operation.amount;
+    } else {
+      return acc - operation.amount
+    }
+  }, 0);
+
+  return balance;
+}
+
+app.post("/account", (request, response) => {
+  const { cpf, name } = request.body;
+
+  const customerAlreadyExists = customers.some((customer) => customer.cpf === cpf);
+
+  if (customerAlreadyExists) {
+    return response.status(400).json({ error: "Customer already exists!" });
+  }
+
+  customers.push({
+    cpf,
+    name,
+    id: uuid(),
+    statement: []
+  });
+
+  return response.status(201).send();
+});
+
+app.get("/statement", verifyAccount, (request, response) => {
+  const { customer } = request;
+
+  return response.json(customer.statement);
+});
+
+app.post("/deposit", verifyAccount, (request, response) => {
+  const { description, amount } = request.body;
+  const { customer } = request;
+
+  const operation = {
+    description,
+    amount,
+    created_at: new Date(),
+    type: "credit"
+  }
+
+  customer.statement.push(operation);
+
+  return response.status(201).send();
+});
+
+app.post("/withdraw", verifyAccount, (request, response) => {
+  const { amount } = request.body;
+  const { customer } = request;
+
+  const balance = getBalance(customer.statement);
+
+  if (balance < amount) {
+    return response.status(400).json({ error: "insuficient founds!" });
+  }
+
+  const operation = {
+    amount,
+    created_at: new Date(),
+    type: "withdraw"
+  }
+
+  customer.statement.push(operation);
+
+  return response.status(201).send();
+});
+
+app.get("/statement/date", verifyAccount, (request, response) => {
+  const { date } = request.query;
+  const { customer } = request;
+
+  const formatDate = new Date(date + ' 00:00');
+
+  const statement = customer.statement.filter(
+    (statement) => statement.created_at.toDateString() ===
+      formatDate.toDateString());
+
+  return response.json(statement);
+});
+
+app.put("/account", verifyAccount, (request, response) => {
+  const { name } = request.body;
+  const { customer } = request;
+
+  customer.name = name;
+
+  return response.status(200).send();
+});
+
+app.get("/account", verifyAccount, (request, response) => {
+  const { customer } = request;
+
+  return response.json(customer);
+});
+
+app.delete("/account", verifyAccount, (request, response) => {
+  const { customer } = request;
+
+  customers.splice(customer, 1);
+
+  return response.status(200).json(customers);
+});
+
+app.get("/balance", verifyAccount, (request, response) => {
+  const { customer } = request;
+
+  const balance = getBalance(customer.statement);
+
+  return response.status(200).json(balance);
+});
+
+app.listen(3333);
